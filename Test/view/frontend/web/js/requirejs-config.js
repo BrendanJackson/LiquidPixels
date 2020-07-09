01430072539c8fb9b589078ea0b908c6b52e5354
brendan <brendan@upperrm.com>
2020-07-09 18:50:19 +0000

    add adminpanel css

---
+++ Test/view/frontend/web/js/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    "map": {
+        '*': {
+            "script_short_name": "Liquid_Test/templates/LiquidPixels"
+        }
+    },
+    "shim": {
+        "Liquid_Test/templates/LiquidPixels": ["jquery"]
+    }
+};
